7704d045a937b3e8c54309bdd4012a1b5e9b17b5
Анастасия Ведь <Lazuritova@yandex.ru>
2015-12-26 15:32:05 +0300

    dds

--- my_project1.c
+++ my_project1.c
@@ -1,25 +1,28 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <menu.h>
 #include <stdlib.h> 
 #include <string.h> 
 #include <dirent.h>
 #include <curses.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 #define CTRLD   4
+#define ENTER 10
 
-void get_choice(char ***choices, char path[], int *n_choices) {
+void new_choice(char ***choices, char path[], int *n_choices) {
     DIR *dir;
     struct dirent *entry;
     dir = opendir(path);
-    int i, j = 0;
+    int j, i = 0;
     while((entry = readdir(dir)) != NULL) {
         i++;
     };
     *choices = (char **)malloc((i + 1)*sizeof(char *));
-    for(j = 0; j < i; j++) *(*choices + j) = (char *)malloc(256*sizeof(char));
+    for(j = 0; j < i; j++) {
+        *(*choices + j) = (char *)malloc(256*sizeof(char));
+    }
     dir = opendir(path);
     int f = 0;
     while ((entry = readdir(dir)) != NULL) {
@@ -30,93 +33,97 @@ void get_choice(char ***choices, char path[], int *n_choices) {
     *n_choices = f + 1;
 } 
 
-void unpost(MENU *menu, ITEM **my_items, int *n_choices) {
-    int i;
-    unpost_menu(menu);
-    free_menu(menu);
-    for(i = 0; i <= n_choices-1; ++i) //
-        free_item(my_items[i]);
-    endwin();
-}
-
-void middle_write(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 
 int main() {
-    struct dirent *entry;
-    struct stat file;
     DIR *dir;
-    ITEM **my_items1, **my_items2;              
+    struct dirent *entry;
+    struct stat lala;
+    ITEM **my_items1, **my_items2;
+    int c;              
     MENU *my_menu1, *my_menu2;
     WINDOW *my_menu_win1, *my_menu_win2;
-    int c;
-    int new_choices1, new_choices2, i, j, current_window = 0;
-    char new_path1[2000] = "/";
-    char new_path2[2000] = "/";
-    char **my_choices1, **my_choices2;
+    int n_choices1, n_choices2, i, j, currwin = 0;
+    char path1[2048] = "/";
+    char path2[2048] = "/";
+    char **choices1;
+    char **choices2;
+    
     initscr();
     start_color();
-    keypad(stdscr, TRUE);
     cbreak();
     noecho();
+    keypad(stdscr, TRUE);
     init_pair(1, COLOR_RED, COLOR_BLACK);
 
-    get_choice(&my_choices1, new_path1, &new_choices1);
-    get_choice(&my_choices2, new_path2, &new_choices2);
-    my_items1 = (ITEM **)calloc(new_choices1, sizeof(ITEM *));
-    my_items2 = (ITEM **)calloc(new_choices2, sizeof(ITEM *));
-    for(i = 0; i < new_choices1; ++i) my_items1[i] = new_item(my_choices1[i], "");
-    for(i = 0; i < new_choices2; ++i) my_items2[i] = new_item(my_choices2[i], "");
+    new_choice(&choices1, path1, &n_choices1);
+    new_choice(&choices2, path2, &n_choices2);
+
+    my_items1 = (ITEM **)calloc(n_choices1, sizeof(ITEM *));
+    for(i = 0; i < n_choices1; ++i)
+        my_items1[i] = new_item(choices1[i], "");
+    my_items2 = (ITEM **)calloc(n_choices2, sizeof(ITEM *));
+    for(i = 0; i < n_choices2; ++i)
+        my_items2[i] = new_item(choices2[i], "");
+
     my_menu1 = new_menu((ITEM **)my_items1);
     my_menu2 = new_menu((ITEM **)my_items2);
-    my_menu_win1 = newwin(22, 39, 1, 1);
-    my_menu_win2 = newwin(22, 39, 1, COLS/2 + 1);
-    keypad(my_menu_win1, TRUE);
-    keypad(my_menu_win2, TRUE);
 
+    my_menu_win1 = newwin(22, 39, 1, 1);
+    keypad(my_menu_win1, TRUE);
+    my_menu_win2 = newwin(22, 39, 1, COLS/2 + 1);
+    keypad(my_menu_win2, TRUE);
+     
     set_menu_win(my_menu1, my_menu_win1);
     set_menu_sub(my_menu1, derwin(my_menu_win1, 17, 38, 3, 1));
     set_menu_format(my_menu1, 17, 1);
     set_menu_win(my_menu2, my_menu_win2);
     set_menu_sub(my_menu2, derwin(my_menu_win2, 17, 38, 3, 1));
     set_menu_format(my_menu2, 17, 1);
+    
     box(my_menu_win1, 0, 0);
     mvwaddch(my_menu_win1, 2, 39, ACS_RTEE);
     box(my_menu_win2, 0, 0);
     mvwaddch(my_menu_win2, 2, 39, ACS_RTEE);
-    mvprintw(LINES - 1, 0, "EXIT = F10");
+    mvprintw(LINES - 1, 0, "F10 to exit");
     refresh();
+
     post_menu(my_menu1);
     wrefresh(my_menu_win1);
     post_menu(my_menu2);
     wrefresh(my_menu_win2);
 
-    while ((c = getch()) != KEY_F(10)) {
-        switch (c) {
+    while((c = getch()) != KEY_F(10)) {
+        switch(c) {
             case KEY_DOWN:
-                if (current_window == 0) menu_driver(my_menu1, REQ_DOWN_ITEM);
-                else menu_driver(my_menu2, REQ_DOWN_ITEM);
+                if (currwin == 0)
+                    menu_driver(my_menu1, REQ_DOWN_ITEM);
+                else
+                    menu_driver(my_menu2, REQ_DOWN_ITEM);
                 break;
             case KEY_UP:
-                if (current_window == 0) menu_driver(my_menu1, REQ_UP_ITEM);
-                else menu_driver(my_menu2, REQ_UP_ITEM);
+                if (currwin == 0)
+                    menu_driver(my_menu1, REQ_UP_ITEM);
+                else
+                    menu_driver(my_menu2, REQ_UP_ITEM);
                 break;
             case 9:
-                current_window = (current_window + 1) % 2;
+                currwin = (currwin + 1) % 2;
                 break;
-            case 10:
-                if (current_window == 0) {
-                    char add[2048];
-                    strcpy(add, new_path1);
-                    strcat(new_path1, "/");
-                    strcat(new_path1, item_name(current_item(my_menu1)));
-                    if (stat(new_path1, &file) >= 0) {
-                        if (S_ISDIR(file.st_mode)) {
-                            DIR *d = opendir(new_path1);
+            case ENTER:
+                if (currwin == 0) {
+                    char helper[2048];
+                    strcpy(helper, path1);
+                    strcat(path1, "/");
+                    strcat(path1, item_name(current_item(my_menu1)));
+                    if (stat(path1, &lala) >= 0) {
+                        if (S_ISDIR(lala.st_mode)) {
+                            DIR *d = opendir(path1);
                             if (d != NULL) {
                                 unpost_menu(my_menu1);
-                                get_choice(&my_choices1, new_path1, &new_choices1);
-                                my_items1 = (ITEM **)calloc(new_choices1, sizeof(ITEM *));
-                                for(i = 0; i < new_choices1; ++i) my_items1[i] = new_item(my_choices1[i], "");
+                                new_choice(&choices1, path1, &n_choices1);
+                                my_items1 = (ITEM **)calloc(n_choices1, sizeof(ITEM *));
+                                for(i = 0; i < n_choices1; ++i) my_items1[i] = new_item(choices1[i], "");
                                 my_menu1 = new_menu((ITEM **)my_items1);
                                 set_menu_win(my_menu1, my_menu_win1);
                                 set_menu_sub(my_menu1, derwin(my_menu_win1, 17, 38, 3, 1));
@@ -127,27 +134,27 @@ int main() {
                                 post_menu(my_menu1);
                                 wrefresh(my_menu_win1);
                             } else {
-                                strcpy(new_path1, add);
+                                strcpy(path1, helper);
                             }
                         } else {
-                            strcpy(new_path1, add);
+                            strcpy(path1, helper);
                         }
                     } else {
-                        strcpy(new_path1, add);
+                        strcpy(path1, helper);
                     }
                 } else {
-                    char add[2048];
-                    strcpy(add, new_path1);
-                    strcat(new_path2, "/");
-                    strcat(new_path2, item_name(current_item(my_menu2)));
-                    if (stat(new_path2, &file) >= 0) {
-                        if (S_ISDIR(file.st_mode)) {
-                            DIR *d = opendir(new_path2);
+                    char helper[2048];
+                    strcpy(helper, path1);
+                    strcat(path2, "/");
+                    strcat(path2, item_name(current_item(my_menu2)));
+                    if (stat(path2, &lala) >= 0) {
+                        if (S_ISDIR(lala.st_mode)) {
+                            DIR *d = opendir(path2);
                             if (d != NULL) {
                                 unpost_menu(my_menu2);
-                                get_choice(&my_choices2, new_path2, &new_choices2);
-                                my_items2 = (ITEM **)calloc(new_choices2, sizeof(ITEM *));
-                                for(i = 0; i < new_choices2; ++i) my_items2[i] = new_item(my_choices2[i], "");
+                                new_choice(&choices2, path2, &n_choices2);
+                                my_items2 = (ITEM **)calloc(n_choices2, sizeof(ITEM *));
+                                for(i = 0; i < n_choices2; ++i) my_items2[i] = new_item(choices2[i], "");
                                 my_menu2 = new_menu((ITEM **)my_items2);
                                 set_menu_win(my_menu2, my_menu_win2);
                                 set_menu_sub(my_menu2, derwin(my_menu_win2, 17, 38, 3, 1));
@@ -158,13 +165,13 @@ int main() {
                                 post_menu(my_menu2);
                                 wrefresh(my_menu_win2);
                             } else {
-                                strcpy(new_path2, add);
+                                strcpy(path2, helper);
                             }
                         } else {
-                            strcpy(new_path2, add);
+                            strcpy(path2, helper);
                         }
                     } else {
-                        strcpy(new_path2, add);
+                        strcpy(path2, helper);
                     }
                 } 
                 break;
@@ -172,6 +179,15 @@ int main() {
         wrefresh(my_menu_win1);
         wrefresh(my_menu_win2); 
     }   
-    unpost(my_menu1, my_items1, new_choices1);
-    unpost(my_menu2, my_items2, new_choices2);
+
+    unpost_menu(my_menu1);
+    free_menu(my_menu1);
+    for(i = 0; i < n_choices1; ++i)
+        free_item(my_items1[i]);
+    endwin();
+    unpost_menu(my_menu2);
+    free_menu(my_menu2);
+    for(i = 0; i < n_choices2; ++i)
+        free_item(my_items2[i]);
+    endwin();
 }
